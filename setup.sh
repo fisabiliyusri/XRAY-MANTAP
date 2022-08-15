e944dcf11f9b643f4304a62d69be60d0183640f0
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 21:58:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,3 +16,41 @@ clear
 
 https://raw.githubusercontent.com/adamfuadia/testxray/main/ins-xray.sh
 #SELESAI
+echo " "
+echo "Installation has been completed!!"echo " "
+echo "============================================================================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "----------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - Nginx                      : 89"  | tee -a log-install.txt
+echo "   - XRAYS TROJAN WS TLS        : 443"  | tee -a log-install.txt
+echo "   - XRAYS SHADOWSOCKS WS TLS   : 443"  | tee -a log-install.txt
+echo "   - XRAYS VLESS WS TLS         : 443"  | tee -a log-install.txt
+echo "   - XRAYS VMESS WS TLS         : 443"  | tee -a log-install.txt
+echo "   - XRAYS TROJAN None TLS      : 80"  | tee -a log-install.txt
+echo "   - XRAYS SHADOWSOCKS None TLS : 80"  | tee -a log-install.txt
+echo "   - XRAYS VLESS None TLS       : 80"  | tee -a log-install.txt
+echo "   - XRAYS VMESS None TLS       : 80"  | tee -a log-install.txt
+echo "   - XRAYS TROJAN GRPC          : 443"  | tee -a log-install.txt
+echo "   - XRAYS SHADOWSOCKS GRPC     : 443"  | tee -a log-install.txt
+echo "   - XRAYS VMESS GRPC           : 443"  | tee -a log-install.txt
+echo "   - XRAYS VLESS GRPC           : 443"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo " Reboot 15 Sec"
+sleep 15
+rm -f setup.sh
