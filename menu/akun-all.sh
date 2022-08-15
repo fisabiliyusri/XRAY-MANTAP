be0f3370f0af6cf30530b6281f7fa04a127f5902
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 19:07:11 +0700

    Create akun-all.sh

---
+++ menu/akun-all.sh
@@ -0,0 +1,41 @@
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "====== XRAY MANTAP Multi Port=======
+echo -e "INFORMASI AKUN VPN XRAY
+echo -e "Host/Domain: $domain
+echo -e "Password/ID: $uuid
+echo -e "Port Websocket TLS: 443
+echo -e "Port Websocket HTTP: 80
+echo -e "Port GRPC TLS: 443
+echo -e "*Note OPOK: opok only supports coremeta
+echo -e "*Note SHADOWSOCKS: gunakan custom config atau plugin xray
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo -e "echo -e "Protokol VPN: TROJAN
+echo -e "Network: WS/GRPC
+echo -e "====== Path =======
+echo -e "=> WS TLS : /xraytrojanws
+echo -e "=> GRPC   : trojan-grpc
+echo -e "=> OPOK : ws://bugcom/xraytrojanws
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo -e "Protokol VPN: SHADOWSOCKS
+echo -e "Network: WS/GRPC
+echo -e "Method Cipers : aes-128-gcm
+echo -e "====== Path =======
+echo -e "=> WS TLS : /xrayssws
+echo -e "=> GRPC   : ss-grpc
+echo -e "=> OPOK : ws://bugcom/xrayssws
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo -e "Protokol VPN: VLESS
+echo -e "Network: WS/GRPC
+echo -e "====== Path =======
+echo -e "=> WS TLS : /xrayws
+echo -e "=> GRPC   : vless-grpc
+echo -e "=> OPOK : ws://bugcom/xrayws
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo -e "Protokol VPN: VMESS
+echo -e "Alter ID: 0
+echo -e "Network: WS/GRPC
+echo -e "====== Path =======
+echo -e "=> WS TLS : /xrayvws
+echo -e "=> GRPC   : vmess-grpc
+echo -e "=> OPOK : ws://bugcom/xrayvws
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
