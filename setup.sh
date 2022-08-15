852d1a4d2aad875e9d4ca97adf345e1fd817e70a
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 22:07:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# =========================================
+cd
 #install tools/alat
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green      Install Tools/Alat               $NC"
