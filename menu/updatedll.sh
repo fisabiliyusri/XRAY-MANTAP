f7009f114448624a43b4c5919426356e62ba5c17
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 23:16:03 +0700

    Create updatedll.sh

---
+++ menu/updatedll.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ==========================================
+# Color
+# download 
+cd /usr/bin
+
+wget -O add-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/add-akun.sh"
+wget -O updatedll "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/updatedll.sh"
+wget -O add-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/add-akun.sh"
+wget -O add-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/add-akun.sh"
+
+
+#
+chmod +x add-akun
+chmod +x delete-akun
+chmod +x updatedll
+chmod +x add-akun
