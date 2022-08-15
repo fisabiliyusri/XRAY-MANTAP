afe44a915cd2002cff73327cc54f53686d05b969
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 18:05:33 +0700

    Update coba-install.sh

--- coba-install.sh
+++ coba-install.sh
@@ -355,6 +355,7 @@ EOF
 
 #
 #
+#nginx config
 cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
              listen 80;
@@ -367,95 +368,9 @@ cat >/etc/nginx/conf.d/xray.conf <<EOF
              ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
              ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
              root /home/vps/public_html;
-location = /xrayws
-{
-proxy_redirect off;
-proxy_pass http://unix:/run/xray/vless_ws.sock;
-proxy_http_version 1.1;
-proxy_set_header X-Real-IP $remote_addr;
-proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-proxy_set_header Upgrade $http_upgrade;
-proxy_set_header Connection "upgrade";
-proxy_set_header Host $http_host;
-}
-location = /xrayvws
-{
-proxy_redirect off;
-proxy_pass http://unix:/run/xray/vmess_ws.sock;
-proxy_http_version 1.1;
-proxy_set_header X-Real-IP $remote_addr;
-proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-proxy_set_header Upgrade $http_upgrade;
-proxy_set_header Connection "upgrade";
-proxy_set_header Host $http_host;
-}
-location = /xraytrojanws
-{
-proxy_redirect off;
-proxy_pass http://unix:/run/xray/trojan_ws.sock;
-proxy_http_version 1.1;
-proxy_set_header X-Real-IP $remote_addr;
-proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-proxy_set_header Upgrade $http_upgrade;
-proxy_set_header Connection "upgrade";
-proxy_set_header Host $http_host;
-}
-location = /xrayssws
-{
-proxy_redirect off;
-proxy_pass http://127.0.0.1:30300;
-proxy_http_version 1.1;
-proxy_set_header X-Real-IP $remote_addr;
-proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-proxy_set_header Upgrade $http_upgrade;
-proxy_set_header Connection "upgrade";
-proxy_set_header Host $http_host;
-}
-location /
-{
-proxy_redirect off;
-proxy_pass http://127.0.0.1:700;
-proxy_http_version 1.1;
-proxy_set_header X-Real-IP $remote_addr;
-proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-proxy_set_header Upgrade $http_upgrade;
-proxy_set_header Connection "upgrade";
-proxy_set_header Host $http_host;
-}
-location ^~ /vless-grpc
-{
-proxy_redirect off;
-grpc_set_header X-Real-IP $remote_addr;
-grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-grpc_set_header Host $http_host;
-grpc_pass grpc://unix:/run/xray/vless_grpc.sock;
-}
-location ^~ /vmess-grpc
-{
-proxy_redirect off;
-grpc_set_header X-Real-IP $remote_addr;
-grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-grpc_set_header Host $http_host;
-grpc_pass grpc://unix:/run/xray/vmess_grpc.sock;
-}
-location ^~ /trojan-grpc
-{
-proxy_redirect off;
-grpc_set_header X-Real-IP $remote_addr;
-grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-grpc_set_header Host $http_host;
-grpc_pass grpc://unix:/run/xray/trojan_grpc.sock;
-}
-location ^~ /ss-grpc
-{
-proxy_redirect off;
-grpc_set_header X-Real-IP $remote_addr;
-grpc_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-grpc_set_header Host $http_host;
-grpc_pass grpc://127.0.0.1:30310;
-}
         }
 EOF
+
 #
 sleep 1
 echo -e "$yell[SERVICE]$NC Restart All service"
