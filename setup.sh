06f06185f04fae465cc184c81ea4c4b721ac2225
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 23:25:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,16 +25,23 @@ echo -e "$green      Install Tools/Alat               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-
 https://raw.githubusercontent.com/adamfuadia/testxray/main/install-tools.sh
 
+#
+#install xmenu
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green      Install xmenu               $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+clear
+
+#
 #Instal Xray
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green          Install XRAY MANTAP              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-
 https://raw.githubusercontent.com/adamfuadia/testxray/main/install-xray.sh
 #SELESAI
 echo " "
