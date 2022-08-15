733e1a239559329b109aca5b8d38b22e26a6fc8a
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 23:47:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ echo -e "$green          Install XRAY MANTAP              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-https://raw.githubusercontent.com/adamfuadia/testxray/main/install-xray.sh
+wget https://raw.githubusercontent.com/adamfuadia/testxray/main/install-xray.sh && chmod +x install-xray.sh && screen -S xray ./install-xray.sh
 #SELESAI
 echo " "
 echo "Installation has been completed!!"echo " "
