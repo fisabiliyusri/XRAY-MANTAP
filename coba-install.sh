3151f53f6a590d919225e30cebe0eb8cc9a50b8a
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 17:57:10 +0700

    Update coba-install.sh

--- coba-install.sh
+++ coba-install.sh
@@ -343,8 +343,8 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStartPre=-/usr/bin/mkdir -p /var/run/xray
-ExecStart=/usr/bin/chown www-data:www-data /var/run/xray
+ExecStartPre=/bin/mkdir -p /var/run/xray
+ExecStart=/bin/chown www-data:www-data /var/run/xray
 Restart=on-failure
 RestartPreventExitStatus=23
 
