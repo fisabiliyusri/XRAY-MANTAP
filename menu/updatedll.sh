5f72bc42cbe8ace3d1ef59acb3f006abb0b96d87
adamfuadia <111186443+adamfuadia@users.noreply.github.com>
2022-08-15 23:36:49 +0700

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -8,10 +8,13 @@ wget -O add-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/men
 wget -O updatedll "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/updatedll.sh"
 wget -O add-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/add-akun.sh"
 wget -O delete-akun "https://raw.githubusercontent.com/adamfuadia/testxray/main/menu/delete-akun.sh"
-
+wget -O certv2ray "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/certv2ray.sh"
 
 #
 chmod +x add-akun
 chmod +x delete-akun
 chmod +x updatedll
 chmod +x add-akun
+chmod +x certv2ray
+
+cd
